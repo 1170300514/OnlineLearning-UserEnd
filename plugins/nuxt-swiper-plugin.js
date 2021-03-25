9e476b475dd3f49b882d945996fa97b8fb14c12d
Haoyu <xyzhang.top@foxmail.com>
2021-03-25 22:12:33 +0800

    init file

---
+++ plugins/nuxt-swiper-plugin.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue'
+// import VueAewsomeSwiper from 'vue-awesome-swiper/dist/ssr'
+
+// Vue.use(VueAewsomeSwiper)
+
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// require styles
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
