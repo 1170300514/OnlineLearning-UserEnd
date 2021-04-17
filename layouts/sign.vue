9d66a2d817a12eff3190b18f4c4f37b588b028df
Haoyu <xyzhang.top@foxmail.com>
2021-04-17 20:12:50 +0800

    登录注册部分结合token验证

--- layouts/sign.vue
+++ layouts/sign.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sign">
     <div class="logo">
-        <img class="logo" src="~/assets/img/logo.png" alt="logo">
+    
     </div>
     <!-- 表单 -->
     <nuxt/>
--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,24 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
+import {MessageBox, Message} from 'element-ui'
 // 创建axios实例
 const service = axios.create({
     baseURL: 'http://localhost:9001', // api的baseuri
     timeout: 20000 // 请求超时时间
 })
+
+// 拦截器
+service.interceptors.request.use(
+    config => {
+        // debugger
+        if(Cookies.get("hit_token")) {
+            // 把获取cookie值放到header中
+            config.headers['token'] = Cookies.get("hit_token");
+        }
+        return config
+    },
+    err => {
+        Promise.reject(err);
+    }
+)
 export default service
